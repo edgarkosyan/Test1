81b9fcdc5589540d2206146dc5c5f25bfd863fd2
edgarkosyan <e.kosyan0073@gmail.com>
2023-11-06 17:42:33 +0400

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Edgar on 06.11.23.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
